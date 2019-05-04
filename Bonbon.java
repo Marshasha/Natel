99b8998867defe80e9d35427a7244e515c287b83
LuyskyHevs <luyet.thomas+hevs@gmail.com>
2019-05-04 10:36:21 +0200

    new file

---
+++ Bonbon.java
@@ -0,0 +1,36 @@
+
+
+
+public class Bonbon {
+
+
+	protected int teneurSucre;
+	
+	public Bonbon(int teneurSucre){
+		
+		this.teneurSucre=teneurSucre;
+		
+	}
+	
+	
+	
+	
+	public int getTeneurSucre() {
+		return teneurSucre;
+	}
+
+
+
+
+	public void setTeneurSucre(int teneurSucre) {
+		this.teneurSucre = teneurSucre;
+	}
+
+
+
+
+	public String toString(){
+		
+		return "";
+	}
+}
